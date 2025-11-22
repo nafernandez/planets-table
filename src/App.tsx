0f375c7eea84331495c6f9ea63f4f9bc4838d4d5
nafernandez <nataliaherenda@gmail.com>
2025-11-21 21:31:53 -0300

    change styles in title

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,10 @@ import { PlanetsTable } from './features/planets'
 function App() {
   return (
     <div className="h-screen flex flex-col overflow-hidden">
-      <div className="flex-shrink-0 px-5 pt-6 pb-4">
-        <h1 className="text-3xl leading-none font-bold px-2 title-planets">
+      <div 
+        className="flex-shrink-0 opacity-100 gap-6 pt-6 pb-6 w-full max-w-[1265px] min-h-[78px] pr-4 sm:pr-[32px] pl-4 sm:pl-[32px] border-b flex flex-wrap items-center"
+      >
+        <h1 className="text-2xl sm:text-3xl leading-tight font-bold px-2 title-planets break-words">
           Planetas y cuerpos celestes
         </h1>
       </div>
