a3d99e15df99f7c13fa2860f165457d7ce68de87
nafernandez <nataliaherenda@gmail.com>
2025-11-21 21:32:41 -0300

    change styles in title

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="h-screen flex flex-col overflow-hidden">
       <div 
-        className="flex-shrink-0 opacity-100 gap-6 pt-6 pb-6 w-full max-w-[1265px] min-h-[78px] pr-4 sm:pr-[32px] pl-4 sm:pl-[32px] border-b flex flex-wrap items-center"
+        className="flex-shrink-0 opacity-100 gap-6 pt-6 pb-6 w-full max-w-[1265px] min-h-[78px] pr-4 sm:pr-[32px] pl-4 sm:pl-[32px] flex flex-wrap items-center"
       >
         <h1 className="text-2xl sm:text-3xl leading-tight font-bold px-2 title-planets break-words">
           Planetas y cuerpos celestes
